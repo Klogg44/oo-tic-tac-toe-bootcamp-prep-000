02ca569d67275bd045b5e64a72be3caa2ccd8016
Spencer Croy <spencerccroy@gmail.com>
2018-11-02 23:15:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,9 +31,9 @@ class TicTacToe
   #   board[index] = current_player
   # end
   #
-  # def position_taken?(board, index)
-  #   !(board[index].nil? || board[index] == " ")
-  # end
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
   #
   # def valid_move?(board, index)
   #   index.between?(0,8) && !position_taken?(board, index)
