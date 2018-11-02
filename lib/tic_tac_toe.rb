e392108c52bcaf267557f78fec44de22d2a828f4
Spencer Croy <spencerccroy@gmail.com>
2018-11-02 23:18:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ class TicTacToe
   def move(index, current_player)
     @board[index] = current_player
   end
-  
+
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end
@@ -38,19 +38,19 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
-  #
-  # def turn(board)
-  #   puts "Please enter 1-9:"
-  #   input = gets.strip
-  #   index = input_to_index(input)
-  #   if valid_move?(board, index)
-  #     move(board, index, current_player(board))
-  #     display_board(board)
-  #   else
-  #     turn(board)
-  #   end
-  # end
-  #
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player
+      display_board
+    else
+      turn
+    end
+  end
+  
   def turn_count
     counter = 0
     @board.each do |space|
