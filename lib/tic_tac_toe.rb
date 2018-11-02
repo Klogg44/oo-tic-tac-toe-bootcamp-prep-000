5f6661108ed0b873304ecd624355eb20e668d327
Spencer Croy <spencerccroy@gmail.com>
2018-11-02 22:58:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,4 +68,25 @@ class TicTacToe
     end
   end
 
+  def won? (board)
+    if board.any?{ |space| space != " " }
+      WIN_COMBINATIONS.each do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+
+        if position_taken?(board, win_index_1)
+          if position_1 == position_2 && position_1 == position_3
+            return win_combination
+          end
+        end
+      end
+    end
+    false
+  end
+
 end
