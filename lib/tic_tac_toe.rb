d82c36fb4b6a4cd179b4e9ba56f2d1bb16849624
Spencer Croy <spencerccroy@gmail.com>
2018-11-02 23:13:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,14 +22,14 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  #
-  # def input_to_index(user_input)
-  #   user_input.to_i - 1
-  # end
-  #
-  # def move(board, index, current_player)
-  #   board[index] = current_player
-  # end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(board, index, current_player)
+    board[index] = current_player
+  end
   #
   # def position_taken?(board, index)
   #   !(board[index].nil? || board[index] == " ")
